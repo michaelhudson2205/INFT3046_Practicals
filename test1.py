a14abef2983f44933ff77ba2d0ca8c85168a586c
Michael Hudson <michaelhudson2205@gmail.com>
2025-09-22 20:32:48 +1000

    Finished week 1 practical activity.

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
-print("Hello Mike. Today is Monday, 22 September 2025.")
+# print("Hello Mike. Today is Monday, 22 September 2025.")
+
+# Ran this to install PyTorch:
+# pip install torch torchvision --index-url https://download.pytorch.org/whl/cu129
+
+# Ran this to install the packages listed in requirements.txt:
+# pip install -r requirements.txt
+
+# Ran this to update pip:
+# python -m pip install --upgrade pip
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
